8c86381856049f240829dcfd974e80e328b15a49
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-20 03:25:08 +0600

    Problem 6 — Role-Based Permission Checker

---
+++ src/canEdit.ts
@@ -0,0 +1,9 @@
+type Role = "admin" | "editor" | "viewer";
+const canEdit=(role:Role): boolean=>{
+    if(role==='admin' || role=="editor") return true;
+    else return false;
+}
+console.log(canEdit("admin"));
+console.log(canEdit("editor"));
+console.log(canEdit("viewer"));
+
