bfd78e173f5e48257ae1d4a3a2cbb7a77a60412f
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-20 02:41:47 +0600

    Problem 4 — Shopping Cart Calculator-using reduce and type-product__> item

--- src/calculateCartTotal.ts
+++ src/calculateCartTotal.ts
@@ -1,11 +1,3 @@
-const calculateCartTotal=(products: Product[]): number=> {
-    // let sum:number = 0;
-    // for(let i=0; i<products.length; i++){
-    //     sum+=products[i].price;
-    // } return sum;
-    return products.reduce((sum, item) => sum + item.price, 0);
-    
-}
 type Product = {
     name: string,
     price: number
@@ -16,3 +8,12 @@ const products: Product[]= [
     { name: "USB Cable", price: 300 } 
 ];
 console.log(calculateCartTotal(products));
+
+const calculateCartTotal=(products: Product[]): number=> {
+    // let sum:number = 0;
+    // for(let i=0; i<products.length; i++){
+    //     sum+=products[i].price;
+    // } return sum;
+    return products.reduce((sum, item) => sum + item.price, 0);
+    
+}
