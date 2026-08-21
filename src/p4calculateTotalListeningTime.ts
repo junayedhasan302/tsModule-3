473ce113fe76d9850d35cdc0dbc6bac7283bae80
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-21 22:45:25 +0600

    Problem 4 — Music App Listening Time Tracker

---
+++ src/p4calculateTotalListeningTime.ts
@@ -0,0 +1,25 @@
+interface Track {
+  title: string;
+  minutes: number;
+}
+const tracks1: Track[] = [
+  { title: "Blinding Lights", minutes: 3 },
+  { title: "Levitating", minutes: 4 },
+  { title: "Peaches", minutes: 3 },
+];
+const tracks2 = [ 
+    { title: "Flowers", minutes: 3 }, 
+    { title: "Anti-Hero", minutes: 4 } 
+];
+
+const calculateTotalListeningTime = (tracks: Track[]): number => {
+  const arr = [...tracks];
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i].minutes;
+  }
+  return sum;
+};
+
+console.log(calculateTotalListeningTime(tracks1));
+console.log(calculateTotalListeningTime(tracks2));
