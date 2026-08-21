ba515b73e90ba2df553432e802663e0794b6e924
junayedhasan302 <junayedhasan302@gmail.com>
2026-08-21 22:21:44 +0600

    Problem 3 — Esports Player Profile Card

---
+++ src/p3formatPlayerCard.ts
@@ -0,0 +1,19 @@
+interface Player{
+    username: string, 
+    level: number, 
+    region: string
+}
+const playerX: Player = { 
+    username: "ShadowByte", 
+    level: 42, 
+    region: "Asia" 
+}
+function formatPlayerCard(player: Player): string { 
+    return `${player.username} is a Level ${player.level} player from ${player.region}.`;
+}
+
+console.log(formatPlayerCard(playerX));
+
+
+// Expected output: 
+// "ShadowByte is a Level 42 player from Asia." 
